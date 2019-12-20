fcfbcf75a4b298e6c8918db19cf3fb1bd5e74894
Ryan Dahl <ry@tinyclouds.org>
2019-12-20 10:58:23 -0500

    first pass at compile_module

--- src/script_compiler.rs
+++ src/script_compiler.rs
@@ -1,4 +1,8 @@
+// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
+//! For compiling scripts.
+use crate::Isolate;
 use crate::Local;
+use crate::Module;
 use crate::ScriptOrigin;
 use crate::String;
 use std::mem::MaybeUninit;
@@ -10,6 +14,13 @@ extern "C" {
     origin: &ScriptOrigin,
   );
   fn v8__ScriptCompiler__Source__DESTRUCT(this: &mut Source);
+
+  fn v8__ScriptCompiler__CompileModule(
+    isoate: &Isolate,
+    source: &Source,
+    options: CompileOptions,
+    no_cache_reason: NoCacheReason,
+  ) -> *mut Module;
 }
 
 #[repr(C)]
@@ -30,5 +41,53 @@ impl Source {
 impl Drop for Source {
   fn drop(&mut self) {
     unsafe { v8__ScriptCompiler__Source__DESTRUCT(self) }
+  }
+}
+
+#[repr(C)]
+pub enum CompileOptions {
+  NoCompileOptions = 0,
+  ConsumeCodeCache,
+  EagerCompile,
+}
+
+/// The reason for which we are not requesting or providing a code cache.
+#[repr(C)]
+pub enum NoCacheReason {
+  NoCacheNoReason = 0,
+  NoCacheBecauseCachingDisabled,
+  NoCacheBecauseNoResource,
+  NoCacheBecauseInlineScript,
+  NoCacheBecauseModule,
+  NoCacheBecauseStreamingSource,
+  NoCacheBecauseInspector,
+  NoCacheBecauseScriptTooSmall,
+  NoCacheBecauseCacheTooCold,
+  NoCacheBecauseV8Extension,
+  NoCacheBecauseExtensionModule,
+  NoCacheBecausePacScript,
+  NoCacheBecauseInDocumentWrite,
+  NoCacheBecauseResourceWithNoCacheHandler,
+  NoCacheBecauseDeferredProduceCodeCache,
+}
+
+/// Compile an ES module, returning a Module that encapsulates the compiled
+/// code.
+///
+/// Corresponds to the ParseModule abstract operation in the ECMAScript
+/// specification.
+pub fn compile_module(
+  isolate: &Isolate,
+  source: Source,
+  options: CompileOptions,
+  no_cache_reason: NoCacheReason,
+) -> Option<Local<Module>> {
+  unsafe {
+    Local::from_raw(v8__ScriptCompiler__CompileModule(
+      isolate,
+      &source,
+      options,
+      no_cache_reason,
+    ))
   }
 }
