6d30c77116a821ffd32d97e279a3ee7810477f9f
Bert Belder <bertbelder@gmail.com>
2019-12-22 16:22:44 +0100

    Set up test harness for verifying safety constraints (#117)

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -807,3 +807,13 @@ fn primitive_array() {
   isolate.exit();
   drop(g);
 }
+
+#[test]
+fn ui() {
+  // This environment variable tells build.rs that we're running trybuild tests,
+  // so it won't rebuild V8.
+  std::env::set_var("DENO_TRYBUILD", "1");
+
+  let t = trybuild::TestCases::new();
+  t.compile_fail("tests/compile_fail/*.rs");
+}
