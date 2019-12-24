4e0b4eccd98596ed8cedeb38158dc7d8658f536b
afinch7 <andyfinch7@gmail.com>
2019-12-24 05:29:23 -0500

    docs

--- src/array_buffer_view.rs
+++ src/array_buffer_view.rs
@@ -20,22 +20,31 @@ extern "C" {
   ) -> usize;
 }
 
+/// A base class for an instance of one of "views" over ArrayBuffer,
+/// including TypedArrays and DataView (ES6 draft 15.13).
 #[repr(C)]
 pub struct ArrayBufferView(Opaque);
 
 impl ArrayBufferView {
+  /// Returns underlying ArrayBuffer.
   pub fn buffer<'sc>(&self) -> Option<Local<'sc, ArrayBuffer>> {
     unsafe { Local::from_raw(v8__ArrayBufferView__Buffer(self)) }
   }
 
+  /// Size of a view in bytes.
   pub fn byte_length(&self) -> usize {
     unsafe { v8__ArrayBufferView__ByteLength(self) }
   }
 
+  /// Byte offset in |Buffer|.
   pub fn byte_offset(&self) -> usize {
     unsafe { v8__ArrayBufferView__ByteOffset(self) }
   }
 
+  /// Copy the contents of the ArrayBufferView's buffer to an embedder defined
+  /// memory without additional overhead that calling ArrayBufferView::Buffer
+  /// might incur.
+  /// Returns the number of bytes actually written.
   pub fn copy_contents(&self, dest: &mut [u8]) -> usize {
     unsafe {
       v8__ArrayBufferView__CopyContents(
