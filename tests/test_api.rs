a31999a2c62cd22323e3f888fb5ba952ab083d83
Ryan Dahl <ry@tinyclouds.org>
2019-12-23 19:10:33 -0500

    remove escape handle scope

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -891,15 +891,11 @@ fn module_evaluation() {
       _referrer: v8::Local<v8::Module>,
     ) -> *mut v8::Module {
       let isolate_: &mut v8::Isolate = context.as_mut();
-      let module_ = {
-        let mut escapable_scope = v8::EscapableHandleScope::new(isolate_);
-        let origin = mock_script_origin(isolate_, "module.js");
-        let source = v8::script_compiler::Source::new(specifier, &origin);
-        let module =
-          v8::script_compiler::compile_module(isolate_, source).unwrap();
-        escapable_scope.escape(cast(module))
-      };
-      &mut *cast(module_)
+      let origin = mock_script_origin(isolate_, "module.js");
+      let source = v8::script_compiler::Source::new(specifier, &origin);
+      let module =
+        v8::script_compiler::compile_module(isolate_, source).unwrap();
+      &mut *cast(module)
     }
     let result = module.instantiate_module(context, resolve_callback);
     assert!(result.unwrap());
