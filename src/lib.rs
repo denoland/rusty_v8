b1a5afbe3aac4d67282f017c78b90912cd4f02a6
Enok <416828041@qq.com>
2019-12-27 18:38:49 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,35 +3,34 @@
 //! # Hello World Example
 //!
 //! ```
-//! use rusty_v8::{Isolate, V8, Allocator, HandleScope, Script, Context, Locker, Local};
-//! use rusty_v8::platform;
+//! use rusty_v8::{Isolate, V8, HandleScope, Script, Context, Locker, Local};
+//! use rusty_v8::{platform, new_default_allocator};
 //!
 //! let platform = platform::new_default_platform();
 //! V8::initialize_platform(platform);
 //! V8::initialize();
 //!
 //! let mut create_params = Isolate::create_params();
-//! create_params.set_array_buffer_allocator(Allocator::new_default_allocator());
+//! create_params.set_array_buffer_allocator(new_default_allocator());
 //! let isolate = Isolate::new(create_params);
 //! let mut locker = Locker::new(&isolate);
+//! let mut handle_scope = HandleScope::new(&mut locker);
+//! let scope = handle_scope.enter();
 //!
-//! HandleScope::enter(&mut locker, |scope| {
-//!   let mut context = Context::new(scope);
-//!   context.enter();
-//!   let code = rusty_v8::String::new(scope, "'Hello World!'").unwrap();
-//!   code.to_rust_string_lossy(scope);
-//!   let mut script = Script::compile(scope, context, code, None).unwrap();
-//!   let result = script.run(scope, context).unwrap();
-//!   let result: Local<rusty_v8::String> = unsafe { std::mem::transmute_copy(&result) };
+//! let mut context = Context::new(scope);
+//! context.enter();
+//! let code = rusty_v8::String::new(scope, "'Hello World!'").unwrap();
+//! code.to_rust_string_lossy(scope);
+//! let mut script = Script::compile(scope, context, code, None).unwrap();
+//! let result = script.run(scope, context).unwrap();
+//! let result: Local<rusty_v8::String> = unsafe { std::mem::transmute_copy(&result) };
 //!
-//!   let str = result.to_rust_string_lossy(scope);
+//! let str = result.to_rust_string_lossy(scope);
 //!
-//!   println!("{}", str);
+//! println!("{}", str);
 //!
-//!   context.exit();
-//! });
+//! context.exit();
 //!
-//! drop(locker);
 //! ```
 //!
 
