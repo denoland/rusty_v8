3b56f46a2ee7829ebb1b98099ff3e117b621d212
afinch7 <andyfinch7@gmail.com>
2019-12-28 11:54:57 -0500

    docs

--- src/shared_array_buffer.rs
+++ src/shared_array_buffer.rs
@@ -21,10 +21,16 @@ extern "C" {
   ) -> SharedRef<BackingStore>;
 }
 
+/// An instance of the built-in SharedArrayBuffer constructor.
+/// This API is experimental and may change significantly.
 #[repr(C)]
 pub struct SharedArrayBuffer(Opaque);
 
 impl SharedArrayBuffer {
+  /// Create a new SharedArrayBuffer. Allocate |byte_length| bytes.
+  /// Allocated memory will be owned by a created SharedArrayBuffer and
+  /// will be deallocated when it is garbage-collected,
+  /// unless the object is externalized.
   pub fn new<'sc>(
     scope: &mut impl ToLocal<'sc>,
     byte_length: usize,
@@ -34,10 +40,15 @@ impl SharedArrayBuffer {
     }
   }
 
+  /// Data length in bytes.
   pub fn byte_length(&self) -> usize {
     unsafe { v8__SharedArrayBuffer__ByteLength(self) }
   }
 
+  /// Get a shared pointer to the backing store of this array buffer. This
+  /// pointer coordinates the lifetime management of the internal storage
+  /// with any live ArrayBuffers on the heap, even across isolates. The embedder
+  /// should not attempt to manage lifetime of the storage through other means.
   pub fn get_backing_store(&self) -> SharedRef<BackingStore> {
     unsafe { v8__SharedArrayBuffer__GetBackingStore(self) }
   }
