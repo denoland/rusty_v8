8fb75f2300cbe6861cf50f4c2f1d2b055470af4f
Ryan Dahl <ry@tinyclouds.org>
2020-01-03 16:17:22 -0500

    SnapshotCreator changes for libdeno

--- src/external_references.rs
+++ src/external_references.rs
@@ -1,19 +1,28 @@
 // Copyright 2019-2020 the Deno authors. All rights reserved. MIT license.
-use crate::function::FunctionCallback;
 use crate::support::intptr_t;
-use std::ffi::c_void;
+use crate::AccessorNameGetterCallback;
+use crate::FunctionCallback;
+use crate::MessageCallback;
+
+#[derive(Clone, Copy)]
+pub union ExternalReference {
+  pub function: FunctionCallback,
+  pub getter: AccessorNameGetterCallback,
+  pub message: MessageCallback,
+}
 
 pub struct ExternalReferences {
-  null_terminated: Vec<*const libc::c_void>,
+  null_terminated: Vec<*const std::ffi::c_void>,
 }
 
 unsafe impl Sync for ExternalReferences {}
 
 impl ExternalReferences {
-  pub fn new(refs: &[FunctionCallback]) -> Self {
+  pub fn new(refs: &[ExternalReference]) -> Self {
     let mut null_terminated = Vec::with_capacity(refs.len() + 1);
     for r in refs {
-      null_terminated.push(*r as *const c_void);
+      let ptr = unsafe { std::mem::transmute(*r) };
+      null_terminated.push(ptr);
     }
     null_terminated.push(std::ptr::null());
     Self { null_terminated }
