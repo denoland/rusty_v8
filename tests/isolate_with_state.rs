b8e627f1d15406144475bc157a9bcb60cc23f736
Ryan Dahl <ry@tinyclouds.org>
2020-02-13 14:41:28 -0500

    state

--- tests/isolate_with_state.rs
+++ tests/isolate_with_state.rs
@@ -1,75 +1,76 @@
 use rusty_v8 as v8;
+use std::ops::{Deref, DerefMut};
+
+struct IsolateWithStateInner<S> {
+  isolate: Option<v8::OwnedIsolate>,
+  state: S,
+}
+
+struct IsolateWithState<S>(Box<IsolateWithStateInner<S>>);
+
+impl<S> IsolateWithState<S> {
+  fn new(mut isolate: v8::OwnedIsolate, state: S) -> IsolateWithState<S> {
+    assert!(isolate.get_data(0).is_null());
+
+    let mut boxed = Box::new(IsolateWithStateInner {
+      isolate: None,
+      state,
+    });
+
+    let ptr = Box::into_raw(boxed);
+    unsafe { isolate.set_data(0, ptr as *mut _) };
+    boxed = unsafe { Box::from_raw(ptr) };
+    boxed.isolate = Some(isolate);
+    IsolateWithState(boxed)
+  }
+
+  fn from(isolate: &mut v8::Isolate) -> &mut S {
+    unsafe { &mut *(isolate.get_data(0) as *mut S) }
+  }
+}
+
+impl<S> Deref for IsolateWithState<S> {
+  type Target = v8::OwnedIsolate;
+
+  fn deref(&self) -> &Self::Target {
+    self.0.isolate.as_ref().unwrap()
+  }
+}
+
+impl<S> DerefMut for IsolateWithState<S> {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    self.0.isolate.as_mut().unwrap()
+  }
+}
+
+fn change_state(
+  scope: v8::FunctionCallbackScope,
+  _args: v8::FunctionCallbackArguments,
+  _rv: v8::ReturnValue,
+) {
+  todo!()
+}
+
+struct State {
+  a: bool,
+  pub context: v8::Global<v8::Context>,
+}
 
 #[test]
 fn isolate_with_state() {
   v8::V8::initialize_platform(v8::new_default_platform());
   v8::V8::initialize();
 
-  struct IsolateWithStateInner<S> {
-    isolate: Option<v8::OwnedIsolate>,
-    state: S,
-  }
-  struct IsolateWithState<S>(Box<IsolateWithStateInner<S>>);
-
-  impl<S> IsolateWithState<S> {
-    fn from(isolate: &mut v8::Isolate) -> &mut S {
-      unsafe { &mut *(isolate.get_data(0) as *mut S) }
-    }
-
-    fn new(state: S) -> IsolateWithState<S> {
-      let mut params = v8::Isolate::create_params();
-      params.set_array_buffer_allocator(v8::new_default_allocator());
-      let mut isolate = v8::Isolate::new(params);
-
-      let mut boxed = Box::new(IsolateWithStateInner {
-        isolate: None,
-        state,
-      });
-
-      let ptr = Box::into_raw(boxed);
-      unsafe { isolate.set_data(0, ptr as *mut _) };
-      boxed = unsafe { Box::from_raw(ptr) };
-      boxed.isolate = Some(isolate);
-      IsolateWithState(boxed)
-    }
-  }
-
-  use std::ops::{Deref, DerefMut};
-  impl<S> Deref for IsolateWithState<S> {
-    type Target = v8::OwnedIsolate;
-
-    fn deref(&self) -> &Self::Target {
-      self.0.isolate.as_ref().unwrap()
-    }
-  }
-
-  impl<S> DerefMut for IsolateWithState<S> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-      self.0.isolate.as_mut().unwrap()
-    }
-  }
-
-  fn change_state(
-    scope: v8::FunctionCallbackScope,
-    _args: v8::FunctionCallbackArguments,
-    _rv: v8::ReturnValue,
-  ) {
-    todo!()
-  }
-
-  struct State {
-    a: bool,
-    pub context: v8::Global<v8::Context>,
-  }
-
-  let setup_guard = setup();
-
   let state = State {
     a: false,
     context: v8::Global::new(),
   };
 
-  let mut isolate_with_state = IsolateWithState::new(state);
+  let mut params = v8::Isolate::create_params();
+  params.set_array_buffer_allocator(v8::new_default_allocator());
+  let isolate = v8::Isolate::new(params);
+
+  let mut isolate_with_state = IsolateWithState::new(isolate, state);
 
   {
     let mut hs = v8::HandleScope::new(isolate_with_state.deref_mut());
