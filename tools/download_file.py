59c982f94c5c6c639f66a404bc20f399e9564897
Bert Belder <bertbelder@gmail.com>
2020-03-17 13:54:42 -0700

    help

--- tools/download_file.py
+++ tools/download_file.py
@@ -49,7 +49,7 @@ def DownloadUrl(url, output_file):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Download librusty_v8.a')
+    parser = argparse.ArgumentParser(description='Download a file')
     parser.add_argument('--filename', help='where to put binary')
     parser.add_argument('--url', help='what url to download')
     args = parser.parse_args()
