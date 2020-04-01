15a40a6596a1341215d79b620e9656e771ca136d
Ryan Dahl <ry@tinyclouds.org>
2020-04-01 19:47:23 -0400

    Merge all inspector bindings into one file

--- src/inspector/v8_stack_trace.rs
+++
@@ -1,9 +0,0 @@
-use crate::support::CxxVTable;
-
-#[repr(C)]
-pub struct V8StackTrace {
-  _cxx_vtable: CxxVTable,
-}
-
-// TODO(bnoordhuis) This needs to be fleshed out more but that can wait
-// until it's actually needed.
