4a870fcccdbb9f705133f8335e4823e04f871a32
Bert Belder <bertbelder@gmail.com>
2020-04-05 05:12:41 +0200

    Re-enable AArch64/ARM64 builds on Github Actions (#343)

--- build.rs
+++ build.rs
@@ -89,7 +89,7 @@ fn build_v8() {
   }
 
   if env::var("TARGET").unwrap() == "aarch64-unknown-linux-gnu" {
-    gn_args.push("target_cpu=arm64".to_string());
+    gn_args.push(r#"target_cpu="arm64""#.to_string());
     gn_args.push("use_sysroot=true".to_string());
     maybe_install_sysroot("arm64");
     maybe_install_sysroot("amd64");
