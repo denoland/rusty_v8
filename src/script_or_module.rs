a9ea69b5d53b033a9dc59b3534e282a2695605b6
Bert Belder <bertbelder@gmail.com>
2020-05-31 19:22:08 +0200

    Add more comments explaining why Local::from_raw() is appropriate (#389)

--- src/script_or_module.rs
+++ src/script_or_module.rs
@@ -18,6 +18,9 @@ impl ScriptOrModule {
   /// The name that was passed by the embedder as ResourceName to the
   /// ScriptOrigin. This can be either a v8::String or v8::Undefined.
   pub fn get_resource_name(&self) -> Local<Value> {
+    // Note: the C++ `v8::ScriptOrModule::GetResourceName()` does not actually
+    // return a local handle, but rather a handle whose lifetime is bound to
+    // the related `ScriptOrModule` object.
     unsafe {
       let ptr = v8__ScriptOrModule__GetResourceName(self);
       Local::from_raw(ptr).unwrap()
@@ -27,6 +30,9 @@ impl ScriptOrModule {
   /// The options that were passed by the embedder as HostDefinedOptions to the
   /// ScriptOrigin.
   pub fn get_host_defined_options(&self) -> Local<PrimitiveArray> {
+    // Note: the C++ `v8::ScriptOrModule::GetHostDefinedOptions()` does not
+    // actually return a local handle, but rather a handle whose lifetime is
+    // bound to the related `ScriptOrModule` object.
     unsafe {
       let ptr = v8__ScriptOrModule__GetHostDefinedOptions(self);
       Local::from_raw(ptr).unwrap()
