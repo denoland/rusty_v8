95b7eaeb731fba411a03d83e8870277e719a41c2
Divy Srivastava <dj.srivastava23@gmail.com>
2020-07-17 10:27:36 +0530

    docs: correct example
    example code had to be inside a entry fn

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,25 @@
 //! ```rust
 //! use rusty_v8 as v8;
 //!
-//! let platform = v8::new_default_platform().unwrap();
-//! v8::V8::initialize_platform(platform);
-//! v8::V8::initialize();
+//! fn main() {
+//!   let platform = v8::new_default_platform().unwrap();
+//!   v8::V8::initialize_platform(platform);
+//!   v8::V8::initialize();
 //!
-//! let isolate = &mut v8::Isolate::new(Default::default());
+//!   let isolate = &mut v8::Isolate::new(Default::default());
 //!
-//! let scope = &mut v8::HandleScope::new(isolate);
-//! let context = v8::Context::new(scope);
-//! let scope = &mut v8::ContextScope::new(scope, context);
+//!   let scope = &mut v8::HandleScope::new(isolate);
+//!   let context = v8::Context::new(scope);
+//!   let scope = &mut v8::ContextScope::new(scope, context);
 //!
-//! let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
-//! println!("javascript code: {}", code.to_rust_string_lossy(scope));
+//!   let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
+//!   println!("javascript code: {}", code.to_rust_string_lossy(scope));
 //!
-//! let mut script = v8::Script::compile(scope, code, None).unwrap();
-//! let result = script.run(scope).unwrap();
-//! let result = result.to_string(scope).unwrap();
-//! println!("result: {}", result.to_rust_string_lossy(scope));
+//!   let mut script = v8::Script::compile(scope, code, None).unwrap();
+//!   let result = script.run(scope).unwrap();
+//!   let result = result.to_string(scope).unwrap();
+//!   println!("result: {}", result.to_rust_string_lossy(scope));
+//! }
 //! ```
 
 #![allow(clippy::missing_safety_doc)]
