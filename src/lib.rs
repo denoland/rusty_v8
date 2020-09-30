e5f56e218c68e6910fc610b8e99d7b08cf6619b4
devsnek <me@gus.host>
2020-09-29 19:20:07 -0500

    add Symbol, Private, BigInt (#466)

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ extern crate libc;
 
 mod array_buffer;
 mod array_buffer_view;
+mod bigint;
 mod context;
 mod data;
 mod date;
@@ -52,6 +53,7 @@ mod object;
 mod platform;
 mod primitive_array;
 mod primitives;
+mod private;
 mod promise;
 mod property_attribute;
 mod proxy;
@@ -62,6 +64,7 @@ mod shared_array_buffer;
 mod snapshot;
 mod string;
 mod support;
+mod symbol;
 mod template;
 mod uint8_array;
 mod value;
@@ -75,6 +78,7 @@ pub mod script_compiler;
 pub mod V8;
 
 pub use array_buffer::*;
+pub use bigint::*;
 pub use data::*;
 pub use exception::*;
 pub use external_references::ExternalReference;
@@ -103,6 +107,7 @@ pub use platform::Task;
 pub use platform::TaskBase;
 pub use platform::TaskImpl;
 pub use primitives::*;
+pub use private::*;
 pub use promise::{PromiseRejectEvent, PromiseRejectMessage, PromiseState};
 pub use property_attribute::*;
 pub use proxy::*;
@@ -120,6 +125,7 @@ pub use support::SharedPtr;
 pub use support::SharedRef;
 pub use support::UniquePtr;
 pub use support::UniqueRef;
+pub use symbol::*;
 pub use template::*;
 
 // TODO(piscisaureus): Ideally this trait would not be exported.
---
+++ src/private.rs
@@ -0,0 +1,63 @@
+use crate::HandleScope;
+use crate::Isolate;
+use crate::Local;
+use crate::Private;
+use crate::String;
+use crate::Value;
+
+extern "C" {
+  fn v8__Private__New(
+    isolate: *mut Isolate,
+    name: *const String,
+  ) -> *const Private;
+  fn v8__Private__ForApi(
+    isolate: *mut Isolate,
+    name: *const String,
+  ) -> *const Private;
+  fn v8__Private__Name(this: *const Private) -> *const Value;
+}
+
+impl Private {
+  /// Create a private symbol. If name is not empty, it will be the description.
+  pub fn new<'s>(
+    scope: &mut HandleScope<'s>,
+    name: Option<Local<String>>,
+  ) -> Local<'s, Private> {
+    unsafe {
+      scope.cast_local(|sd| {
+        v8__Private__New(
+          sd.get_isolate_ptr(),
+          name.map_or_else(std::ptr::null, |v| &*v),
+        )
+      })
+    }
+    .unwrap()
+  }
+
+  /// Retrieve a global private symbol. If a symbol with this name has not
+  /// been retrieved in the same isolate before, it is created.
+  /// Note that private symbols created this way are never collected, so
+  /// they should only be used for statically fixed properties.
+  /// Also, there is only one global name space for the names used as keys.
+  /// To minimize the potential for clashes, use qualified names as keys,
+  /// e.g., "Class#property".
+  pub fn for_api<'s>(
+    scope: &mut HandleScope<'s>,
+    name: Option<Local<String>>,
+  ) -> Local<'s, Private> {
+    unsafe {
+      scope.cast_local(|sd| {
+        v8__Private__ForApi(
+          sd.get_isolate_ptr(),
+          name.map_or_else(std::ptr::null, |v| &*v),
+        )
+      })
+    }
+    .unwrap()
+  }
+
+  /// Returns the print name string of the private symbol, or undefined if none.
+  pub fn name<'s>(&self, scope: &mut HandleScope<'s>) -> Local<'s, Value> {
+    unsafe { scope.cast_local(|_| v8__Private__Name(&*self)) }.unwrap()
+  }
+}
