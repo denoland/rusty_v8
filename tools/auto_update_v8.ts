12229ab84a14441b71e241afd433669cc50ba70c
Luca Casonato <lucacasonato@yahoo.com>
2021-04-26 13:52:52 +0200

    refs/heads/

--- tools/auto_update_v8.ts
+++ tools/auto_update_v8.ts
@@ -64,7 +64,7 @@ await run(["git", "add", "v8", "README.md"]);
 await run(["git", "commit", "-m", `Rolling to V8 ${newVersion}`]);
 
 // Push to the `denoland/rusty_v8#autoroll`
-await run(["git", "push", "origin", `+HEAD:${AUTOROLL_BRANCH}`]);
+await run(["git", "push", "origin", `+HEAD:refs/heads/${AUTOROLL_BRANCH}`]);
 
 const proc = Deno.run({
   cmd: ["gh", "pr", "view", AUTOROLL_BRANCH],
