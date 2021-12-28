b3e09e69a06ef501aceb37c2707c67d3b0ef8251
Aaron O'Mullan <aaron.omullan@gmail.com>
2021-12-28 16:52:12 +0100

    feat: v8::StackTrace::CurrentStackTrace() bindings (#800)

--- src/exception.rs
+++ src/exception.rs
@@ -1,5 +1,7 @@
 #![allow(non_snake_case)]
 
+use std::convert::TryInto;
+
 use crate::isolate::Isolate;
 use crate::support::int;
 use crate::Context;
@@ -32,6 +34,10 @@ extern "C" {
   fn v8__Message__IsOpaque(this: *const Message) -> bool;
   fn v8__Message__GetStackTrace(this: *const Message) -> *const StackTrace;
 
+  fn v8__StackTrace__CurrentStackTrace(
+    isolate: *mut Isolate,
+    frame_limit: int,
+  ) -> *const StackTrace;
   fn v8__StackTrace__GetFrameCount(this: *const StackTrace) -> int;
   fn v8__StackTrace__GetFrame(
     this: *const StackTrace,
@@ -67,6 +73,19 @@ extern "C" {
 }
 
 impl StackTrace {
+  /// Grab a snapshot of the current JavaScript execution stack.
+  pub fn current_stack_trace<'s>(
+    scope: &mut HandleScope<'s>,
+    frame_limit: usize,
+  ) -> Option<Local<'s, StackTrace>> {
+    let frame_limit = frame_limit.try_into().ok()?;
+    unsafe {
+      scope.cast_local(|sd| {
+        v8__StackTrace__CurrentStackTrace(sd.get_isolate_ptr(), frame_limit)
+      })
+    }
+  }
+
   /// Returns the number of StackFrames.
   pub fn get_frame_count(&self) -> usize {
     unsafe { v8__StackTrace__GetFrameCount(self) as usize }
