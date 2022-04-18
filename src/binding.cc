c585bdd5dd9da4208ee4bb5c231bbf2dba27a074
legendecas <legendecas@gmail.com>
2022-04-19 01:25:56 +0800

    feat: add binding for Context::SetPromiseHooks

--- src/binding.cc
+++ src/binding.cc
@@ -1560,6 +1560,18 @@ const v8::Data* v8__Context__GetDataFromSnapshotOnce(v8::Context& self,
       ptr_to_local(&self)->GetDataFromSnapshotOnce<v8::Data>(index));
 }
 
+void v8__Context__SetPromiseHooks(v8::Context& self,
+                                  v8::Function& init_hook,
+                                  v8::Function& before_hook,
+                                  v8::Function& after_hook,
+                                  v8::Function& resolve_hook) {
+  ptr_to_local(&self)->SetPromiseHooks(
+    ptr_to_local(&init_hook),
+    ptr_to_local(&before_hook),
+    ptr_to_local(&after_hook),
+    ptr_to_local(&resolve_hook));
+}
+
 const v8::String* v8__Message__Get(const v8::Message& self) {
   return local_to_ptr(self.Get());
 }
