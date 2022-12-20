bba9e68a9ea1145f0c0a26531354c6ff888f90f8
linbingquan <695601626@qq.com>
2022-12-20 08:06:25 +0800

    fix: clippy error

--- src/external.rs
+++ src/external.rs
@@ -17,7 +17,8 @@ extern "C" {
 
 impl External {
   #[inline(always)]
-  pub unsafe fn new<'s>(
+  #[allow(clippy::not_unsafe_ptr_arg_deref)]
+  pub fn new<'s>(
     scope: &mut HandleScope<'s, ()>,
     value: *mut c_void,
   ) -> Local<'s, Self> {
