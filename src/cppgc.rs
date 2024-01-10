e0376f0275f38ec3a192ccced63addd896522ac5
Divy Srivastava <dj.srivastava23@gmail.com>
2024-01-10 09:33:27 +0530

    x

--- src/cppgc.rs
+++ src/cppgc.rs
@@ -1,4 +1,4 @@
-// Copyright 2020-2021 the Deno authors. All rights reserved. MIT license
+// Copyright 2019-2021 the Deno authors. All rights reserved. MIT license
 
 use crate::platform::Platform;
 use crate::support::int;
