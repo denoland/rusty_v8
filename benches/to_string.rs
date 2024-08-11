bab43640c05c0bf7b04f255b42fc49511538055b
Divy Srivastava <dj.srivastava23@gmail.com>
2024-08-11 18:52:46 +0530

    Add fast seqonebytestring

--- benches/to_string.rs
+++ benches/to_string.rs
@@ -82,12 +82,57 @@ fn main() {
     global.set(scope, name.into(), func.into()).unwrap();
   }
 
+  {
+    fn fast_fn(
+      _recv: v8::Local<v8::Object>,
+      data: *const v8::fast_api::FastApiOneByteString,
+    ) -> u32 {
+      let data = unsafe { &*data }.as_bytes();
+      data.len() as u32
+    }
+    const FAST_CALL: v8::fast_api::FastFunction =
+      v8::fast_api::FastFunction::new(
+        &[
+          v8::fast_api::Type::V8Value,
+          v8::fast_api::Type::SeqOneByteString,
+        ],
+        v8::fast_api::CType::Uint32,
+        fast_fn as _,
+      );
+    let template = v8::FunctionTemplate::builder(
+      |scope: &mut v8::HandleScope,
+       arg: v8::FunctionCallbackArguments,
+       mut rv: v8::ReturnValue| {
+        let first = arg.get(0);
+        let string = first.to_string(scope).unwrap();
+        let view = v8::ValueView::new(scope, string);
+        if let v8::ValueViewData::OneByte(data) = view.data() {
+          if data.is_ascii() {
+            rv.set_uint32(data.len() as u32);
+            return;
+          }
+        };
+
+        let mut buffer = [std::mem::MaybeUninit::<u8>::uninit(); 1024];
+        let out = string.to_rust_cow_lossy(scope, &mut buffer);
+
+        rv.set_uint32(out.len() as u32);
+      },
+    )
+    .build_fast(scope, &FAST_CALL, None, None, None);
+    let name = v8::String::new(scope, "fast_seqonebytestr").unwrap();
+    let value = template.get_function(scope).unwrap();
+
+    global.set(scope, name.into(), value.into()).unwrap();
+  }
+
   let runs = 100_000_000;
 
   for x in &[
     "to_rust_cow_lossy",
     "to_rust_cow_lossy_stack_buffer",
     "valueview",
+    "fast_seqonebytestr",
   ] {
     let code = format!(
       "
