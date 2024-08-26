c6681026d918eb752320403be894e2f8e51580fd
Nathan Whitaker <nathan@deno.com>
2024-08-26 14:18:59 -0700

    Value deserializer re-entrant

--- src/value_deserializer.rs
+++ src/value_deserializer.rs
@@ -33,19 +33,18 @@ pub struct CxxValueDeserializerDelegate {
 
 #[no_mangle]
 pub unsafe extern "C" fn v8__ValueDeserializer__Delegate__ReadHostObject(
-  this: &mut CxxValueDeserializerDelegate,
+  this: &CxxValueDeserializerDelegate,
   isolate: *mut Isolate,
 ) -> *const Object {
-  let value_deserializer_heap = ValueDeserializerHeap::dispatch_mut(this);
+  let value_deserializer_heap = ValueDeserializerHeap::dispatch(this);
   let scope = &mut CallbackScope::new(isolate.as_mut().unwrap());
   let context = value_deserializer_heap.context.get(scope).unwrap();
   let scope = &mut ContextScope::new(scope, context);
-  let value_deserializer_impl =
-    value_deserializer_heap.value_deserializer_impl.as_mut();
-  match value_deserializer_impl.read_host_object(
-    scope,
-    &mut value_deserializer_heap.cxx_value_deserializer,
-  ) {
+
+  match value_deserializer_heap
+    .value_deserializer_impl
+    .read_host_object(scope, &value_deserializer_heap.cxx_value_deserializer)
+  {
     None => std::ptr::null(),
     Some(x) => x.as_non_null().as_ptr(),
   }
@@ -53,18 +52,17 @@ pub unsafe extern "C" fn v8__ValueDeserializer__Delegate__ReadHostObject(
 
 #[no_mangle]
 pub unsafe extern "C" fn v8__ValueDeserializer__Delegate__GetSharedArrayBufferFromId(
-  this: &mut CxxValueDeserializerDelegate,
+  this: &CxxValueDeserializerDelegate,
   isolate: *mut Isolate,
   transfer_id: u32,
 ) -> *const SharedArrayBuffer {
-  let value_deserializer_heap = ValueDeserializerHeap::dispatch_mut(this);
+  let value_deserializer_heap = ValueDeserializerHeap::dispatch(this);
   let scope = &mut CallbackScope::new(isolate.as_mut().unwrap());
   let context = value_deserializer_heap.context.get(scope).unwrap();
   let scope = &mut ContextScope::new(scope, context);
 
-  let value_deserializer_impl =
-    value_deserializer_heap.value_deserializer_impl.as_mut();
-  match value_deserializer_impl
+  match value_deserializer_heap
+    .value_deserializer_impl
     .get_shared_array_buffer_from_id(scope, transfer_id)
   {
     None => std::ptr::null(),
@@ -78,13 +76,15 @@ pub unsafe extern "C" fn v8__ValueDeserializer__Delegate__GetWasmModuleFromId(
   isolate: *mut Isolate,
   clone_id: u32,
 ) -> *const WasmModuleObject {
-  let value_deserializer_heap = ValueDeserializerHeap::dispatch_mut(this);
+  let value_deserializer_heap = ValueDeserializerHeap::dispatch(this);
   let scope = &mut CallbackScope::new(isolate.as_mut().unwrap());
   let context = value_deserializer_heap.context.get(scope).unwrap();
   let scope = &mut ContextScope::new(scope, context);
-  let value_deserializer_impl =
-    value_deserializer_heap.value_deserializer_impl.as_mut();
-  match value_deserializer_impl.get_wasm_module_from_id(scope, clone_id) {
+
+  match value_deserializer_heap
+    .value_deserializer_impl
+    .get_wasm_module_from_id(scope, clone_id)
+  {
     None => std::ptr::null(),
     Some(x) => x.as_non_null().as_ptr(),
   }
@@ -165,9 +165,9 @@ extern "C" {
 /// custom callback functions used by v8.
 pub trait ValueDeserializerImpl: GarbageCollected {
   fn read_host_object<'s>(
-    &mut self,
+    &self,
     scope: &mut HandleScope<'s>,
-    _value_deserializer: &mut dyn ValueDeserializerHelper,
+    _value_deserializer: &dyn ValueDeserializerHelper,
   ) -> Option<Local<'s, Object>> {
     let msg =
       String::new(scope, "Deno deserializer: read_host_object not implemented")
@@ -178,7 +178,7 @@ pub trait ValueDeserializerImpl: GarbageCollected {
   }
 
   fn get_shared_array_buffer_from_id<'s>(
-    &mut self,
+    &self,
     scope: &mut HandleScope<'s>,
     _transfer_id: u32,
   ) -> Option<Local<'s, SharedArrayBuffer>> {
@@ -193,7 +193,7 @@ pub trait ValueDeserializerImpl: GarbageCollected {
   }
 
   fn get_wasm_module_from_id<'s>(
-    &mut self,
+    &self,
     scope: &mut HandleScope<'s>,
     _clone_id: u32,
   ) -> Option<Local<'s, WasmModuleObject>> {
@@ -206,6 +206,10 @@ pub trait ValueDeserializerImpl: GarbageCollected {
     scope.throw_exception(exc);
     None
   }
+}
+
+fn cast_to_ptr<T>(t: &T) -> *mut T {
+  t as *const _ as *mut _
 }
 
 /// The ValueDeserializerHeap object contains all objects related to a
@@ -246,15 +250,6 @@ impl<'a> ValueDeserializerHeap<'a> {
     Self::get_cxx_value_deserializer_delegate_offset()
       .to_embedder::<Self>(value_serializer_delegate)
   }
-
-  /// Starting from 'this' pointer the ValueDeserializerHeap mut ref can be
-  /// created
-  pub unsafe fn dispatch_mut(
-    value_serializer_delegate: &mut CxxValueDeserializerDelegate,
-  ) -> &mut Self {
-    Self::get_cxx_value_deserializer_delegate_offset()
-      .to_embedder_mut::<Self>(value_serializer_delegate)
-  }
 }
 
 impl<'a> Drop for ValueDeserializerHeap<'a> {
@@ -269,12 +264,12 @@ impl<'a> Drop for ValueDeserializerHeap<'a> {
 /// Mostly used by the read_host_object callback function in the
 /// ValueDeserializerImpl trait to create custom deserialization logic.
 pub trait ValueDeserializerHelper {
-  fn get_cxx_value_deserializer(&mut self) -> &mut CxxValueDeserializer;
+  fn get_cxx_value_deserializer(&self) -> &CxxValueDeserializer;
 
-  fn read_header(&mut self, context: Local<Context>) -> Option<bool> {
+  fn read_header(&self, context: Local<Context>) -> Option<bool> {
     unsafe {
       v8__ValueDeserializer__ReadHeader(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         context,
       )
     }
@@ -282,49 +277,49 @@ pub trait ValueDeserializerHelper {
   }
 
   fn read_value<'s>(
-    &mut self,
+    &self,
     context: Local<'s, Context>,
   ) -> Option<Local<'s, Value>> {
     unsafe {
       Local::from_raw(v8__ValueDeserializer__ReadValue(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         context,
       ))
     }
   }
 
-  fn read_uint32(&mut self, value: &mut u32) -> bool {
+  fn read_uint32(&self, value: &mut u32) -> bool {
     unsafe {
       v8__ValueDeserializer__ReadUint32(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         value,
       )
     }
   }
 
-  fn read_uint64(&mut self, value: &mut u64) -> bool {
+  fn read_uint64(&self, value: &mut u64) -> bool {
     unsafe {
       v8__ValueDeserializer__ReadUint64(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         value,
       )
     }
   }
 
-  fn read_double(&mut self, value: &mut f64) -> bool {
+  fn read_double(&self, value: &mut f64) -> bool {
     unsafe {
       v8__ValueDeserializer__ReadDouble(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         value,
       )
     }
   }
 
-  fn read_raw_bytes(&mut self, length: usize) -> Option<&[u8]> {
+  fn read_raw_bytes(&self, length: usize) -> Option<&[u8]> {
     let mut data: *const c_void = std::ptr::null_mut();
     let ok = unsafe {
       v8__ValueDeserializer__ReadRawBytes(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         length,
         &mut data,
       )
@@ -338,43 +333,43 @@ pub trait ValueDeserializerHelper {
   }
 
   fn transfer_array_buffer(
-    &mut self,
+    &self,
     transfer_id: u32,
     array_buffer: Local<ArrayBuffer>,
   ) {
     unsafe {
       v8__ValueDeserializer__TransferArrayBuffer(
-        self.get_cxx_value_deserializer(),
+        cast_to_ptr(self.get_cxx_value_deserializer()),
         transfer_id,
         array_buffer,
       )
     };
   }
 
-  fn get_wire_format_version(&mut self) -> u32 {
+  fn get_wire_format_version(&self) -> u32 {
     unsafe {
-      v8__ValueDeserializer__GetWireFormatVersion(
+      v8__ValueDeserializer__GetWireFormatVersion(cast_to_ptr(
         self.get_cxx_value_deserializer(),
-      )
+      ))
     }
   }
 }
 
 impl ValueDeserializerHelper for CxxValueDeserializer {
-  fn get_cxx_value_deserializer(&mut self) -> &mut CxxValueDeserializer {
+  fn get_cxx_value_deserializer(&self) -> &CxxValueDeserializer {
     self
   }
 }
 
 impl<'a> ValueDeserializerHelper for ValueDeserializerHeap<'a> {
-  fn get_cxx_value_deserializer(&mut self) -> &mut CxxValueDeserializer {
-    &mut self.cxx_value_deserializer
+  fn get_cxx_value_deserializer(&self) -> &CxxValueDeserializer {
+    &self.cxx_value_deserializer
   }
 }
 
 impl<'a> ValueDeserializerHelper for ValueDeserializer<'a> {
-  fn get_cxx_value_deserializer(&mut self) -> &mut CxxValueDeserializer {
-    &mut self.value_deserializer_heap.cxx_value_deserializer
+  fn get_cxx_value_deserializer(&self) -> &CxxValueDeserializer {
+    &self.value_deserializer_heap.cxx_value_deserializer
   }
 }
 
@@ -385,6 +380,9 @@ impl<'a> ValueDeserializerHelper for ValueDeserializer<'a> {
 /// a Local<'s, Context> for the CallbackScopes
 pub struct ValueDeserializer<'a> {
   value_deserializer_heap: Pin<Box<ValueDeserializerHeap<'a>>>,
+  // ValueDeserializerHeap is already !Send and !Sync
+  // but this is just making it explicit
+  _phantom: std::marker::PhantomData<*mut ()>,
 }
 
 impl<'a> GarbageCollected for ValueDeserializer<'a> {
@@ -401,56 +399,65 @@ impl<'a> ValueDeserializer<'a> {
   ) -> Self {
     let context = scope.get_current_context();
     // create dummy ValueDeserializerHeap and move to heap + pin to address
-    let mut value_deserializer_heap = Box::pin(ValueDeserializerHeap {
-      value_deserializer_impl,
-      cxx_value_deserializer: CxxValueDeserializer {
-        _cxx_vtable: CxxVTable(std::ptr::null()),
-      },
-      cxx_value_deserializer_delegate: CxxValueDeserializerDelegate {
-        _cxx_vtable: CxxVTable(std::ptr::null()),
-      },
-      context: TracedReference::new(scope, context),
-    });
+    let value_deserializer_heap_ptr =
+      Box::into_raw(Box::new(ValueDeserializerHeap {
+        value_deserializer_impl,
+        cxx_value_deserializer: CxxValueDeserializer {
+          _cxx_vtable: CxxVTable(std::ptr::null()),
+        },
+        cxx_value_deserializer_delegate: CxxValueDeserializerDelegate {
+          _cxx_vtable: CxxVTable(std::ptr::null()),
+        },
+        context: TracedReference::new(scope, context),
+      }));
 
     unsafe {
+      let delegate_ptr = std::ptr::addr_of_mut!(
+        (*value_deserializer_heap_ptr).cxx_value_deserializer_delegate
+      );
+      let deserializer_ptr = std::ptr::addr_of_mut!(
+        (*value_deserializer_heap_ptr).cxx_value_deserializer
+      );
       v8__ValueDeserializer__Delegate__CONSTRUCT(
-        &mut value_deserializer_heap.cxx_value_deserializer_delegate
-          as *mut CxxValueDeserializerDelegate
-          as *mut std::mem::MaybeUninit<CxxValueDeserializerDelegate>,
+        delegate_ptr
+          .cast::<std::mem::MaybeUninit<CxxValueDeserializerDelegate>>(),
       );
 
       v8__ValueDeserializer__CONSTRUCT(
-        &mut value_deserializer_heap.cxx_value_deserializer
-          as *mut CxxValueDeserializer
-          as *mut std::mem::MaybeUninit<CxxValueDeserializer>,
+        deserializer_ptr.cast::<std::mem::MaybeUninit<CxxValueDeserializer>>(),
         scope.get_isolate_ptr(),
         data.as_ptr(),
         data.len(),
-        &mut value_deserializer_heap.cxx_value_deserializer_delegate,
+        delegate_ptr,
       );
     };
 
+    // SAFETY: pointer from Box::into_raw is valid
+    let value_deserializer_heap =
+      Pin::new(unsafe { Box::from_raw(value_deserializer_heap_ptr) });
+
     ValueDeserializer {
       value_deserializer_heap,
+      _phantom: std::marker::PhantomData,
     }
   }
 }
 
 impl<'a> ValueDeserializer<'a> {
   pub fn set_supports_legacy_wire_format(
-    &mut self,
+    &self,
     supports_legacy_wire_format: bool,
   ) {
     unsafe {
       v8__ValueDeserializer__SetSupportsLegacyWireFormat(
-        &mut self.value_deserializer_heap.cxx_value_deserializer,
+        cast_to_ptr(&self.value_deserializer_heap.cxx_value_deserializer),
         supports_legacy_wire_format,
       );
     }
   }
 
   pub fn read_value<'t>(
-    &mut self,
+    &self,
     context: Local<'t, Context>,
   ) -> Option<Local<'t, Value>> {
     self.value_deserializer_heap.read_value(context)
