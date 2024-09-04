17893f0f4e4f94c6d1c4a97fca2afbdda674b105
snek <the@snek.dev>
2024-09-03 23:15:30 -0700

    download ninja/gn from cipd

--- tools/setup_rbe.py
+++ tools/setup_rbe.py
@@ -14,13 +14,7 @@ NINJA=autoninja
 ```
 """
 
-Str = str
-def Var(name):
-    if name == 'rbe_instance':
-        return 'projects/rbe-chromium-untrusted/instances/default_instance'
-    return vars[name]
-with open('./v8/DEPS') as f:
-    exec(f.read())
+from v8_deps import Var, hooks
 
 import subprocess
 import os
