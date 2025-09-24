cef30539185c0788383ef917ab102181a3653413
cheesycod <cheesycod@outlook.com>
2025-09-24 02:30:10 +0200

    fix clippy

--- tests/test_sandbox_use.rs
+++ tests/test_sandbox_use.rs
@@ -3,12 +3,12 @@ mod test_sandbox_use {
   #[test]
   #[cfg(feature = "v8_enable_pointer_compression")]
   fn test_sandbox_on() {
-    assert_eq!(true, v8::V8::is_sandboxed());
+    assert!(v8::V8::is_sandboxed());
   }
 
   #[test]
   #[cfg(not(feature = "v8_enable_pointer_compression"))]
   fn test_sandbox_off() {
-    assert_eq!(false, v8::V8::is_sandboxed());
+    assert!(!v8::V8::is_sandboxed());
   }
 }
